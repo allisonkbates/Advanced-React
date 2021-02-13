26943a8e941bf79bd8d8be5a4129b232c9b7d954
Allison <allisonkbates@gmail.com>
2021-02-13 12:18:48 -0500

    halfway thru video 37

--- sick-fits/frontend/lib/paginationField.js
+++ sick-fits/frontend/lib/paginationField.js
@@ -4,7 +4,6 @@ export default function paginationField() {
   return {
     keyArgs: false, // tells apollo we will take care of everything
     read(existing = [], { args, cache }) {
-      console.log({ existing, args, cache });
       const { skip, first } = args;
       // read the number of items on the page from the cache
       const data = cache.readQuery({ query: PAGINATION_QUERY });
@@ -13,7 +12,7 @@ export default function paginationField() {
       const pages = Math.ceil(count / first);
       // check if we have exisitng items
       const items = existing.slice(skip, skip + first).filter((x) => x);
-      if (items.length && items !== first && page === pages) {
+      if (items.length && items.length !== first && page === pages) {
         return items;
       }
       if (items.length !== first) {
@@ -21,19 +20,16 @@ export default function paginationField() {
         return false;
       }
       if (items.length) {
-        console.log(`There are ${items.length}`);
         return items;
       }
       return false; // fallback to the network
     },
     merge(existing, incoming, { args }) {
       const { skip, first } = args;
-      console.log(`merging items from the network ${incoming.length}`);
       const merged = existing ? existing.slice(0) : [];
-      for (let i = 0; i < skip + incoming.length; ++i) {
+      for (let i = skip; i < skip + incoming.length; ++i) {
         merged[i] = incoming[i - skip];
       }
-      console.log(merged);
       // return merged items from the cache
       return merged;
     },
