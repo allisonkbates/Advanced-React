a6b571c98f3243878aa41923e2510ac546ac6235
Allison <allisonkbates@gmail.com>
2021-02-14 15:56:47 -0500

    video 40 & 41

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -1,14 +1,17 @@
-import { createAuth } from '@keystone-next/auth';
-import { config, createSchema } from '@keystone-next/keystone/schema';
-import { withItemData, statelessSessions } from '@keystone-next/keystone/session';
-import { User } from './schemas/User';
-import { Product } from './schemas/Product';
-import { ProductImage } from './schemas/ProductImage';
-import 'dotenv/config';
-import { insertSeedData } from './seed-data';
+import { createAuth } from "@keystone-next/auth";
+import { config, createSchema } from "@keystone-next/keystone/schema";
+import {
+  withItemData,
+  statelessSessions,
+} from "@keystone-next/keystone/session";
+import { User } from "./schemas/User";
+import { Product } from "./schemas/Product";
+import { ProductImage } from "./schemas/ProductImage";
+import "dotenv/config";
+import { insertSeedData } from "./seed-data";
 
 const databaseURL =
-  process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
+  process.env.DATABASE_URL || "mongodb://localhost/keystone-sick-fits-tutorial";
 
 const sessionConfig = {
   maxAge: 60 * 60 * 24 * 360, // how long they stay signed in?
@@ -16,46 +19,52 @@ const sessionConfig = {
 };
 
 const { withAuth } = createAuth({
-  listKey: 'User',
-  identityField: 'email',
-  secretField: 'password',
+  listKey: "User",
+  identityField: "email",
+  secretField: "password",
   initFirstItem: {
-    fields: ['name', 'email', 'password'],
+    fields: ["name", "email", "password"],
     // TODO: Add in roles here
-  }
+  },
+  passwordResetLink: {
+    async sendToken(args) {
+      console.log(args);
+    },
+  },
 });
 
-export default withAuth(config({
-  server: {
-    cors: {
-      origin: [process.env.FRONTEND_URL],
-      credentials: true,
+export default withAuth(
+  config({
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
     },
-  },
-  db: {
-    adapter: 'mongoose',
-    url: databaseURL,
-    async onConnect({ keystone }) {
-      console.log('Connected to the database!');
-      if (process.argv.includes('--seed-data')) {
-        await insertSeedData(keystone);
-      }
+    db: {
+      adapter: "mongoose",
+      url: databaseURL,
+      async onConnect({ keystone }) {
+        console.log("Connected to the database!");
+        if (process.argv.includes("--seed-data")) {
+          await insertSeedData(keystone);
+        }
+      },
+      // TODO: Add data seeding here
     },
-    // TODO: Add data seeding here
-  },
-  lists: createSchema({
-    User,
-    Product,
-    ProductImage
-  }),
-  ui: {
-    // Show the UI for only those who pass this test
-    isAccessAllowed: ({ session }) =>
-      !!session?.data,
-  },
-  session: withItemData(statelessSessions(sessionConfig), {
-    // Graph QL query
-    User: 'id name email'
+    lists: createSchema({
+      User,
+      Product,
+      ProductImage,
+    }),
+    ui: {
+      // Show the UI for only those who pass this test
+      isAccessAllowed: ({ session }) => !!session?.data,
+    },
+    session: withItemData(statelessSessions(sessionConfig), {
+      // Graph QL query
+      User: "id name email",
+    }),
+    // TODO: add session values here
   })
-  // TODO: add session values here
-}));
+);
